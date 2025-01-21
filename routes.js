084a17aec8c1cf52c8202789ecb1408d084f3a64
yuting <guoyuting1year@gmail.com>
2025-01-21 12:03:08 +0800

    Database post retrieval

--- routes.js
+++ routes.js
@@ -1,6 +1,16 @@
 import express from "express";
-import posts from './dummy-post.js';
+import {main,Post} from './models/posts.js';
+let posts = [];
+main();
+let cursor = Post.find().cursor();
+let document;
+while ((document = await cursor.next()) !== null){
+    console.log(document);
+    console.log(typeof document); //object
+    posts.push(document);
 
+}
+console.log(posts);
 
 var app = express();
 const port = 3000;
@@ -8,11 +18,15 @@ app.use(express.static('public'));
 
 
 app.get("/",(req,res)=>{ //homepage
+    
     res.render("home.ejs",{
         recentPosts:posts.slice(0,3)
     })
 })
 
+app.get("/about-me",(req,res)=>{
+    res.render("about-me.ejs");
+})
 
 app.listen(port,(err)=>{
     if (err){
