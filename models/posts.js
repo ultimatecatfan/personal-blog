809281fa69ccf6cb332f5c73bb73f357b1d14210
yuting <guoyuting1year@gmail.com>
2025-01-26 12:08:42 +0800

    Added error page, search functionality and search results page

--- models/posts.js
+++ models/posts.js
@@ -5,6 +5,7 @@ let posts = [];
 const postShema = new mongoose.Schema({
     date: {type: Date, default: Date.now() }, //sets default date
     title: String,
+    slug: String,
     image: String,
     body: String
 })
@@ -30,12 +31,14 @@ main()
 
 posts.push(new Post({
     title: "Teaching Assistant for LLB Law of Torts class",
+    slug: "teaching-assistant-torts",
     image: "law-of-torts",
     body: "I am pleased to announce that I have received a teaching position in SMU!<br>I thoroughly enjoyed learning the Law of Torts when I was taking the module. It was fascinating learning about what Prof Jerrold Soh termed the law of misadventures. I was fascinated by the diversity of cases which we had to read as part of the module, as they covered a range of fact situations from road accidents to exorcisms (yes, literally), medical negligence, and so much more.<br> I hope that the students who are in the class I am helping to teach will thoroughly enjoy their time in class as well."
 }));
 
 posts.push(new Post({
     title:"A review of Giovanni's room",
+    slug: "review-giovannis-room",
     image:"giovanni's-room",
     body: "Giovanni's room is an absolutely fantastic work. It is not so much a story about David's sexuality but rather his inability to love anyone -- he cannot love Giovanni or Hella; and, most importantly, he is unable to love himself. David's story is marked by shame. Convinced that he would eventually become his drunken and possibly womanising father, David follows in his footsteps and too ends up drowning his sorrows at the bottom of a bottle. Ashamed of his budding bisexuality as a child, David abandons Joey. He loves Giovanni, and the two of them have an intense sexual and emotional relationship, yet he is terrified to commit fully to Giovanni and too leaves him, sealing Giovanni's tragic fate.<br> Significantly, when David expresses his desire to leave Paris and Giovanni behind in order to start a 'safe' life with a woman he does not particularly love, David says that he wishes to leave Giovanni's room -- that place where the two had made loved, fought, reconciled in. Throughout the entire novel, David adopts various methods of escape: drinking, pointless casual sex, and -- most destructive of all -- fleeing from those who love him.<br> I loved Giovanni as a character. He is beautiful, passionate and vulnerable. Whereas David is repressed, Giovanni is seemingly shameless and openly homosexual. However, very early on, we see glimpses of Giovanni's sadness. He does not much want to live, expressing to David that he wished to die many times before David arrived, and remarking that both the world and himself were dirty."
 }))
--- routes.js
+++ routes.js
@@ -1,6 +1,9 @@
 import express from "express";
 import {main,Post} from './models/posts.js';
 import {aboutInfo} from './models/about-info.js';
+import bodyParser from 'body-parser';
+
+
 
 main();
 
@@ -18,6 +21,7 @@ async function getPosts(){
 var app = express();
 const port = 3000;
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended:true}));
 
 async function getDescription(){ //function to get the aboutinfo 
     try{
@@ -27,6 +31,38 @@ async function getDescription(){ //function to get the aboutinfo
     }
     catch(err){
         console.log(`There is an error in retrieving the about! ${err}`);
+    }
+
+}
+
+async function getSinglePostFromSearch(query){
+    try{
+        let matchingPosts = [];
+        let allPosts = await getPosts();
+        allPosts.forEach(post=>{
+            if (post.title.toLowerCase().includes(query.toLowerCase())){
+                //if the post title contains the query
+                matchingPosts.push(post); 
+            }
+            else if (post.body.toLowerCase().includes(query.toLowerCase())){
+                matchingPosts.push(post);
+            }
+
+        })
+        return matchingPosts;
+    }
+    catch(err){
+        console.err(`An error occurred when trying to find the posts you were looking for. ${err}`)
+    }
+}
+
+async function getSinglePost(postSlug){
+    try{
+        let foundPost = await Post.findOne({slug:postSlug});
+        return foundPost;
+    }
+    catch(err){
+        console.err(`Error loading the single post with matching title from the database! ${err}`);
     }
 
 }
@@ -55,6 +91,44 @@ app.get("/about-me",async (req,res)=>{
     catch(err){
         console.log(`error in rendering the about-me page with the required description. ${err}`);
     }
+})
+
+app.get("/all-posts",async(req,res)=>{
+    try{
+        let allPosts = await getPosts(); //all the posts from the database retrieved
+        res.render("all-posts.ejs",{
+            posts:allPosts
+        });
+    }
+    catch(err){
+        console.log(`error when trying to return the all posts page: ${err}`);
+    }
+})
+
+app.get("/:postSlug",async(req,res)=>{ //parameter: :postTitle
+    try{
+        let foundPost = await getSinglePost(req.params['postSlug']);
+        if (foundPost){
+            console.log("There is a post that matches the post title.");
+        }
+        res.render("post.ejs",{post:foundPost});
+    }
+    catch(err){
+        console.log("Error in loading the single post matching the post title.")
+    }
+})
+
+app.post('/search',async(req,res)=>{
+    let query = req.body.search;
+    let matchingPosts = await getSinglePostFromSearch(query);
+    if (matchingPosts.length > 0){
+        res.render('found-posts.ejs',{posts:matchingPosts, length:matchingPosts.length}
+        );
+    }
+    else{
+        res.render('error-404.ejs');
+    }
+
 })
 
 app.listen(port,(err)=>{
